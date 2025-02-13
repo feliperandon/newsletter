e0bc66d11e1a6ee3abb260add4d34f43d2fc3eae
feliperandon <feliperandon32@gmail.com>
2025-02-13 14:51:02 -0300

    structuring form component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,17 @@ import Form from "./components/Form";
 
 function App() {
   return (
-    <>
-      <h1 className="text-red-600">Hello</h1>
-      <Form />
-    </>
+    <div>
+      <h1 className="text-red-600">Increva-se</h1>
+      <p>Assine nosso Newsletter e mantenha-se informado</p>
+      <div>
+        <Form />
+      </div>
+      <p>
+        Ao se inscrever, você passará a receber os nossos emails com as melhores
+        dicas, novidades e ofertas
+      </p>
+    </div>
   );
 }
 
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <form>
+      <div>
+        <label>Nome</label>
+        <input type="text" placeholder="Digite o seu nome" />
+      </div>
+      <div>
+        <label>Email</label>
+        <input type="email" placeholder="Digite o seu email" />
+      </div>
+      <div>
+        <a href="Ler os termos"></a>
+        <div>
+          <input type="checkbox" />
+          <label>Concordo com os termos</label>
+        </div>
+      </div>
+      <button>Cadastrar</button>
+    </form>
+  );
 };
 
 export default Form;
