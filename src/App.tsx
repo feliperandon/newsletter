a494bf33dd7d1cc796bd577f7f678fbf443e00a9
feliperandon <feliperandon32@gmail.com>
2025-02-13 14:45:21 -0300

    starting project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-600">Hello</h1>
+      <Form />
+    </>
+  );
+}
+
+export default App;
